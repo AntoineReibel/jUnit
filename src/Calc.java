bacf623bdf6c96da9c2bbc0c8f0b1467433acd96
Antoine <antoine.reib3l@gmail.com>
2024-04-05 09:44:54 +0200

    add sub test

--- src/Calc.java
+++ src/Calc.java
@@ -2,4 +2,8 @@ public class Calc {
     public static int add(int number1, int number2){
         return number1 + number2;
     }
+
+    public static int subtract(int number1, int number2){
+        return number1 - number2;
+    }
 }
--- src/CalcTest.java
+++ src/CalcTest.java
@@ -6,7 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalcTest {
     @Test
     @DisplayName("add two number")
-void add(){
+    void add() {
         assertEquals(4, Calc.add(2, 2));
+    }
+
+    @Test
+    @DisplayName("subtract two number")
+    void subtract() {
+        assertEquals(0, Calc.subtract(2, 2));
     }
 }
